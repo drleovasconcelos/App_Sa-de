aee1af3e4119bf53f6a3b8a3f92ff8fe66286112
Leonardo <111660177+drleovasconcelos@users.noreply.github.com>
2024-11-28 22:03:19 -0300

    Delete APP SAÚDE directory

--- APP SAÚDE/agendei-mobile/src/constants/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://seu-ip-aqui:3001"
-});
-
-export default api;
--- APP SAÚDE/agendei-web/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles/global.css";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import Rotas from "./rotas.jsx";
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Rotas />
-)
